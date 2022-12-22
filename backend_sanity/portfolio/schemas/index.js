f57e55d144aa0d539259d0d1763a589d82e43806
sahar samer <saharabdalazeez97@email.com>
2022-12-22 15:31:53 +0200

    addin multiple schemas

--- backend_sanity/portfolio/schemas/index.js
+++ backend_sanity/portfolio/schemas/index.js
@@ -1,3 +1,17 @@
 import testimonials from './testimonials'
+import brands from './brands'
+import contact from './contact'
+import experiences from './experiences'
+import skills from './skills'
+import workExperience from './workExperience'
+import works from './works'
 
-export const schemaTypes = [testimonials]
+export const schemaTypes = [
+  testimonials,
+  brands,
+  contact,
+  experiences,
+  skills,
+  workExperience,
+  works,
+]
