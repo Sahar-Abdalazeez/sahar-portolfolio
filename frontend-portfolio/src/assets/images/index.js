92eb615b0d6a1ff637c8d75e7e3bc6fe1dffced3
sahar samer <saharabdalazeez97@email.com>
2022-12-23 17:28:04 +0200

    adding images

--- frontend-portfolio/src/assets/images/index.js
+++ frontend-portfolio/src/assets/images/index.js
@@ -8,3 +8,7 @@ export { default as html } from "./html.png";
 export { default as javascript } from "./javascript.png";
 export { default as css } from "./css.png";
 export { default as react } from "./react.png";
+export { default as about01 } from "./about01.png";
+export { default as about02 } from "./about02.png";
+export { default as about03 } from "./about03.png";
+export { default as about04 } from "./about04.png";
--- frontend-portfolio/src/container/About/About.jsx
+++ frontend-portfolio/src/container/About/About.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './About.scss';
+import { motion } from 'framer-motion'
+import { images } from '../../assets';
+import { urlFor, client } from '../../client';
 
 function About() {
+    const [abouts, setAbouts] = useState([]);
+    //we want to call this useEffect at the start once when the code loads 
+    useEffect(() => {
+        const query = '*[_type == "abouts"]';
+        client.fetch(query).then((data) => setAbouts(data))
+    }, [])
+
+    console.log('abouts', abouts)
     return (
-        <div>About</div>
+        <>
+            <h2 className='head-text' > I Know that <span>Good Apps</span><br />means <span>Good Business</span> </h2>
+
+            <div className='app__profiles'>
+                {abouts.map((about, index) => (
+                    <motion.div
+                        whileInView={{ opacity: 1 }}
+                        whileHover={{ scale: 1.1 }}
+                        transition={{ duration: 0.5, type: 'tween' }}
+                        className="app__profile-item"
+                        key={about.title + index}
+                    >
+                        <img src={urlFor(about.imgUrl)} alt={about.title} />
+                        <h2 className='bold-text' style={{ marginTop: 20 }} >{about.title}</h2>
+                        <p className='p-text' style={{ marginTop: 10 }} >{about.description}</p>
+
+                    </motion.div>
+                ))}
+
+            </div>
+        </>
     )
 }
 
