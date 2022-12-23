0692e44bca9fc5ae53a9b8838965cae68ee200b8
sahar samer <saharabdalazeez97@email.com>
2022-12-23 22:42:32 +0200

    fix header name

--- frontend-portfolio/src/container/Header/Header.jsx
+++ frontend-portfolio/src/container/Header/Header.jsx
@@ -27,13 +27,13 @@ const Header = () => (
                     <span>👋</span>
                     <div style={{ marginLeft: 20 }}>
                         <p className="p-text">Hello, I am</p>
-                        <h1 className="head-text">Micael</h1>
+                        <h1 className="head-text">SAHAR</h1>
                     </div>
                 </div>
 
                 <div className="tag-cmp app__flex">
-                    <p className="p-text">Web Developer</p>
-                    <p className="p-text">Freelancer</p>
+                    <p className="p-text">Frontend Developer</p>
+                    <p className="p-text">Web and Mobile using<br />React and React native </p>
                 </div>
             </div>
         </motion.div>
